e287b1e26b69ac4cf2dc92aac1c33d5ab1ab00b9
Isabelle Cristina <110576073+isabellecristinav@users.noreply.github.com>
2022-11-24 20:09:35 -0300

    continuação do formulário

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ function Salvar(){
     let nome = document.getElementById('nome').value
     let sobrenome = document.getElementById('sobrenome').value
     let email = document.getElementById('email').value
-    let linha = '<tr><td>' + nome + '</td><td>' + sobrenome + '</td><td>' + email + '</td></tr>'
+    let linha = '<tr><td>' + nome + '</td><td>' + sobrenome + '</td><td>' + email + '</td><td>' + senha + '</td><td>' + confirmacao + '</td><td>' + data + '</td><td>' + endereco + '</td><td>' + numero + '</td><td>' + cidade + '</td><td>' + estado + '</td></tr>'
     document.getElementById('clientes').innerHTML +=linha
 }
